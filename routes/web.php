7f09443c43e987f495c5c97de18a5ec76528bc01
Nika Jorjoliani <nikajorjika1993@gmail.com>
2025-02-04 11:54:17 +0100

    Feat: Add empty controllers and routes for save card and subscribe functionalities

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\InitiatePaymentController;
+use App\Http\Controllers\SaveCardController;
+use App\Http\Controllers\SubscribeController;
 use App\Http\Controllers\TransactionStatusController;
 use Illuminate\Support\Facades\Route;
 
 Route::any('/initiate', InitiatePaymentController::class)->name('initiate');
+Route::any('/save-card', SaveCardController::class)->name('save-card');
+Route::any('/subscribe', SubscribeController::class)->name('subscribe');
 Route::any('/status/{transaction_id}', TransactionStatusController::class)->name('status');
